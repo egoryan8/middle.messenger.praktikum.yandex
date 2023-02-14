f2857ef1a6e5930501fed64de53f4e2cc5874c7b
Fogel Egor <fogel_92@bk.ru>
2023-02-14 17:09:58 +0700

    refactor: update server, variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(express.static('./dist'));
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + './dist/index.html')
 })
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 })
