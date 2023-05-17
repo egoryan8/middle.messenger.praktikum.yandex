3add7f47291d80745e0d4ebdaee2b3e9491fd38d
Fogel Egor <fogel_92@bk.ru>
2023-05-17 20:51:01 +0700

    refactor: typing http request class

--- src/utils/HTTPRequest.ts
+++ src/utils/HTTPRequest.ts
@@ -1,4 +1,4 @@
-enum Methods {
+enum METHODS {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
@@ -12,27 +12,38 @@ function queryStringify(data: XMLHttpRequestBodyInit) {
 }
 
 interface IOptions {
-  method?: Methods;
+  method?: METHODS;
   timeout?: number;
   headers?: Record<string, string>;
   data?: XMLHttpRequestBodyInit;
 }
 
+type HTTPMethod = (url: string, options: IOptions) => Promise<unknown>;
+
 export class HTTPTransport {
-  get = (url: string, options: IOptions = {}) => this.request(url, { ...options, method: Methods.GET }, options.timeout);
+  // используем тип и удаляем дублирование в аргументах
+  get: HTTPMethod = (url, options) => (
+    this.request(url, { ...options, method: METHODS.GET }, options.timeout)
+  );
+  // используем тип и удаляем дублирование в аргументах
+  put: HTTPMethod = (url, options) => (
+    this.request(url, { ...options, method: METHODS.PUT }, options.timeout)
+  );
+  // используем тип и удаляем дублирование в аргументах
+  post: HTTPMethod = (url, options) => (
+    this.request(url, { ...options, method: METHODS.POST }, options.timeout)
+  );
+  // используем тип и удаляем дублирование в аргументах
+  delete: HTTPMethod = (url, options) => (
+    this.request(url, { ...options, method: METHODS.DELETE }, options.timeout)
+  );
 
-  post = (url: string, options: IOptions = {}) => this.request(url, { ...options, method: Methods.POST }, options.timeout);
-
-  put = (url: string, options: IOptions = {}) => this.request(url, { ...options, method: Methods.PUT }, options.timeout);
-
-  delete = (url: string, options: IOptions = {}) => this.request(url, { ...options, method: Methods.DELETE }, options.timeout);
-
-  request = (url: string, options: IOptions = { method: Methods.GET }, timeout = 5000) => {
+  request = (url: string, options: IOptions = { method: METHODS.GET }, timeout = 5000) => {
     const { headers = {}, method, data } = options;
 
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open(method as string, method === Methods.GET && !!data ? url + queryStringify(data) : url);
+      xhr.open(method as string, method === METHODS.GET && !!data ? url + queryStringify(data) : url);
 
       Object.keys(headers).forEach((key) => {
         xhr.setRequestHeader(key, headers[key]);
@@ -47,7 +58,7 @@ export class HTTPTransport {
       xhr.ontimeout = reject;
       xhr.timeout = timeout;
 
-      if (method === Methods.GET || !data) {
+      if (method === METHODS.GET || !data) {
         xhr.send();
       } else {
         xhr.send(data as XMLHttpRequestBodyInit);
