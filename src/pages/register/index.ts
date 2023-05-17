76d07f475726dd4dbd07b6d833d8e4520e2feb2d
Fogel Egor <fogel_92@bk.ru>
2023-05-17 22:06:20 +0700

    refactor: validation to register page

--- src/pages/register/index.ts
+++ src/pages/register/index.ts
@@ -3,10 +3,12 @@ import { RegisterPage } from './register';
 import { registerComponent } from '../../utils/registerComponent';
 import { InputWithLabel } from '../../components/input-with-label';
 import { Button } from '../../components/button';
+import { Input } from '../../components/input';
 
 import './index.scss';
 
 registerComponent(Button, 'Button');
+registerComponent(Input, 'Input');
 registerComponent(InputWithLabel, 'InputWithLabel');
 
 const registerPage = new RegisterPage();
--- src/pages/register/register.template.ts
+++ src/pages/register/register.template.ts
@@ -1,15 +1,18 @@
+import {
+  REGEXP_EMAIL, REGEXP_LOGIN, REGEXP_NAME, REGEXP_PASSWORD, REGEXP_PHONE,
+} from '../../utils/regexps';
+
 export default `
 <div class="register-page">
   <div class="register-page__wrapper">
     <h1 class="register-page__title">Регистрация</h1>
     <div class="register-page__input-wrapper">
-      {{{ InputWithLabel label="Почта" type="email" placeholder="Почта" errorText="Неверная почта" name="email"}}}
-      {{{ InputWithLabel label="Логин" type="text" placeholder="Логин" name="login"}}}
-      {{{ InputWithLabel label="Имя" type="text" placeholder="Имя" name="first_name"}}}
-      {{{ InputWithLabel label="Фамилия" type="text" placeholder="Фамилия" name="second_name"}}}
-      {{{ InputWithLabel label="Телефон" type="text" placeholder="Телефон" name="phone"}}}
-      {{{ InputWithLabel label="Пароль" type="password" placeholder="Пароль" name="password"}}}
-      {{{ InputWithLabel label="Пароль (ещё раз)" type="password" placeholder="Пароль (ещё раз)" name="password"}}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Почта" errorText="Латиница, обязательно должен быть символ @" inputType="email" inputName="email" regexp="${REGEXP_EMAIL}" }}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Логин" errorText="Должно содержать от 3 до 8 символов" inputId="login" inputType="text" inputName="login" regexp="${REGEXP_LOGIN}" }}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Имя" errorText="Первая буква должна быть заглавной, без пробелов и без цифр" inputId="first_name" inputType="text" inputName="first_name" regexp="${REGEXP_NAME}" }}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Фамилия"  errorText="Первая буква должна быть заглавной, без пробелов и без цифр"   inputId="second_name" inputType="text" inputName="second_name" regexp="${REGEXP_NAME}" }}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Телефон" errorText="Цифры, от 11 до 15 символов" inputId="phone" inputType="tel" inputName="phone" regexp="${REGEXP_PHONE}" }}}
+                        {{{ InputWithLabel classInput="input-with-label__input" labelText="Пароль" errorText="Должно содержать от 8 до 40 символов + Заглавный символ" inputId="password" inputType="password" inputName="password" regexp="${REGEXP_PASSWORD}" }}}
     </div>
     <div class="register-page__button-wrapper">
       {{{ Button text="Зарегистрироваться"}}}
